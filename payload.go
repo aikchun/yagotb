48db71ba1fc5cf2a36fd8807ac177e4b706e6082
aikchun <iam@simaikchun.com>
2022-08-19 12:41:37 +0800

    issues/1/fix-omitempty: Fixed omitempty syntax.

--- payload.go
+++ payload.go
@@ -3,9 +3,9 @@ package telegrambot
 type SendMessagePayload struct {
 	ChatId           int64                `json:"chat_id"`
 	Text             string               `json:"text"`
-	ReplyToMessageID int64                `json:"reply_to_message_id", omitempty`
-	ParseMode        string               `json:"parse_mode", omitempty`
-	ReplyMarkup      InlineKeyboardMarkup `json:"reply_markup", omitempty`
+	ReplyToMessageID int64                `json:"reply_to_message_id,omitempty"`
+	ParseMode        string               `json:"parse_mode,omitempty"`
+	ReplyMarkup      InlineKeyboardMarkup `json:"reply_markup,omitempty"`
 }
 
 type InlineKeyboardMarkup struct {
@@ -14,8 +14,8 @@ type InlineKeyboardMarkup struct {
 
 type InlineKeyboardButton struct {
 	Text                         string `json:"text"`
-	Url                          string `json:"url", omitempty`
-	CallbackData                 string `json:"callback_data", omitempty`
-	SwitchInlineQuery            string `json:"switch_inline_query", omitempty`
-	SwitchInlineQueryCurrentChat string `json:"switch_inline_query_current_chat", omitempty`
+	Url                          string `json:"url,omitempty"`
+	CallbackData                 string `json:"callback_data,omitempty"`
+	SwitchInlineQuery            string `json:"switch_inline_query,omitempty"`
+	SwitchInlineQueryCurrentChat string `json:"switch_inline_query_current_chat,omitempty"`
 }
