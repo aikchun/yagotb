d37a1908274ec720f96205809ed52b4fad3512a9
aikchun <iam@simaikchun.com>
2022-08-20 00:56:40 +0800

    issues/13/renameid: renamed all id

--- payload.go
+++ payload.go
@@ -1,7 +1,7 @@
 package telegrambot
 
 type SendMessagePayload struct {
-	ChatId           int64                 `json:"chat_id"`
+	ChatID           int64                 `json:"chat_id"`
 	Text             string                `json:"text"`
 	ReplyToMessageID int64                 `json:"reply_to_message_id,omitempty"`
 	ParseMode        string                `json:"parse_mode,omitempty"`
